d12ade0c5f833c91208e5965234c0a559aad4f5d
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-08-27 23:59:10 +0200

    MapActivity

--- app/src/main/java/it/uniupo/livelight/search/MapActivity.kt
+++ app/src/main/java/it/uniupo/livelight/search/MapActivity.kt
@@ -1,13 +1,15 @@
 package it.uniupo.livelight.search
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.android.gms.maps.MapsInitializer
 import it.uniupo.livelight.R
 
 class MapActivity : AppCompatActivity() {
+    private val REQUEST_CODE_LOCATION = 300
     private lateinit var map: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,14 @@ class MapActivity : AppCompatActivity() {
         map = findViewById(R.id.map)
         map.onCreate(savedInstanceState)
         map.onResume()
+
+        // Requests location permission
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_CODE_LOCATION
+        )
+
+        MapsInitializer.initialize(applicationContext)
 
         // Back button
         val actionBar = supportActionBar
