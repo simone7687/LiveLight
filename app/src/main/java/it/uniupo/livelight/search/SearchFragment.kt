f90a5701a53f5f949f497cea6477149f5a55fd70
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-08-27 12:26:31 +0200

    SearchFragment (find post by distance)

--- app/src/main/java/it/uniupo/livelight/search/SearchFragment.kt
+++ app/src/main/java/it/uniupo/livelight/search/SearchFragment.kt
@@ -1,15 +1,26 @@
 package it.uniupo.livelight.search
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.firestore.FirebaseFirestore
 import it.uniupo.livelight.R
+import it.uniupo.livelight.post.PostListAdapter
+import it.uniupo.livelight.post.PostModel
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
+    private val db = FirebaseFirestore.getInstance()
 
-    private lateinit var searchViewModel: SearchViewModel
+    private var distanceSelected: Int = 0
+    var lastLocation: Location? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +28,106 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_search, container, false)
+
+        root.spinner_distance.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                // p2: position
+                distanceSelected = p2
+                // Last position
+                // TODO: Requesting permits
+                val fusedLocationClient =
+                    LocationServices.getFusedLocationProviderClient(requireActivity())
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    // Got last known location
+                    if (location != null) {
+                        lastLocation = location
+                    } else if (p2 != 0)
+                        Toast.makeText(
+                            activity?.baseContext, R.string.no_location,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                }
+                // Update the list
+                if (lastLocation != null)
+                    updatePostList(
+                        root.findViewById(R.id.list_post),
+                        distanceSelected,
+                        lastLocation
+                    )
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        updatePostList(root.findViewById(R.id.list_post), distanceSelected, lastLocation)
+
         return root
+    }
+
+    private fun updatePostList(postList: ListView, x: Int, lastLocation: Location?) {
+        db.collection(getString(R.string.db_post)).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // for PostListAdapter
+                    val titlePost: ArrayList<String> = ArrayList()
+                    val descriptionPost: ArrayList<String> = ArrayList()
+                    val imagePost: ArrayList<String> = ArrayList()
+
+                    loop@ for (item in task.result!!.documents) {
+                        val model = PostModel(item.id)
+                        model.user = item.get(getString(R.string.db__userId)) as String
+                        model.title = item.get(getString(R.string.db__title)) as String
+                        model.description = item.get(getString(R.string.db__description)) as String
+                        model.image = item.get(getString(R.string.db__imageUrl)) as String
+
+                        // Find only posts of the selected distance
+                        if (x != 0 && lastLocation != null) {
+                            val itemLoc = Location("")
+                            itemLoc.latitude = model.coordinates[0]
+                            itemLoc.longitude = model.coordinates[1]
+
+                            when (x) {
+                                //10km
+                                1 -> {
+                                    if (lastLocation.distanceTo(itemLoc) > 10000)
+                                        continue@loop
+                                }
+                                //20km
+                                2 -> {
+                                    if (lastLocation.distanceTo(itemLoc) > 20000)
+                                        continue@loop
+                                }
+                                //50km
+                                3 -> {
+                                    if (lastLocation.distanceTo(itemLoc) > 50000)
+                                        continue@loop
+                                }
+                            }
+                        }
+
+                        // for PostListAdapter
+                        titlePost.add(model.title)
+                        descriptionPost.add(model.description)
+                        imagePost.add(model.image)
+                    }
+
+                    // Update the list of posts
+                    postList.adapter =
+                        PostListAdapter(
+                            this.requireActivity(),
+                            titlePost,
+                            descriptionPost,
+                            imagePost
+                        )
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(
+                    activity?.baseContext, exception.localizedMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
